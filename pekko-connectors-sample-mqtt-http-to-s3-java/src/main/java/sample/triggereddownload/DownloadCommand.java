e46cc43df116a7f1c4e0fea8a5241551fd03a149
laglang <tangjiafu@corp.netease.com>
2023-12-17 16:04:47 +0800

    [mqtt-http-to-s3-java] Migrate akka dependencies to pekko

--- pekko-connectors-sample-mqtt-http-to-s3-java/src/main/java/sample/triggereddownload/DownloadCommand.java
+++ pekko-connectors-sample-mqtt-http-to-s3-java/src/main/java/sample/triggereddownload/DownloadCommand.java
@@ -6,17 +6,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Instant;
 
 public final class DownloadCommand {
-    public final Instant timestamp;
-    public final String url;
 
-    @JsonCreator
-    public DownloadCommand(@JsonProperty("timestamp") Instant timestamp, @JsonProperty("url") String url) {
-        this.timestamp = timestamp;
-        this.url = url;
-    }
+  public final Instant timestamp;
+  public final String url;
 
-    @Override
-    public String toString() {
-        return "DownloadCommand(" + timestamp.toString() + ", url=" + url + ")";
-    }
+  @JsonCreator
+  public DownloadCommand(@JsonProperty("timestamp") Instant timestamp, @JsonProperty("url") String url) {
+    this.timestamp = timestamp;
+    this.url = url;
+  }
+
+  @Override
+  public String toString() {
+    return "DownloadCommand(" + timestamp.toString() + ", url=" + url + ")";
+  }
 }
--- pekko-connectors-sample-mqtt-http-to-s3-java/src/main/java/sample/triggereddownload/Main.java
+++ pekko-connectors-sample-mqtt-http-to-s3-java/src/main/java/sample/triggereddownload/Main.java
@@ -26,59 +26,62 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
 
-    final ActorSystem<Void> system;
-    final Http http;
+  final ActorSystem<Void> system;
+  final Http http;
 
-    public static void main(String[] args) throws Exception {
-        Main me = new Main();
-        me.run();
-    }
+  public static void main(String[] args) throws Exception {
+    Main me = new Main();
+    me.run();
+  }
 
-    public Main() {
-        system = ActorSystem.create(Behaviors.empty(), "MqttHttpToS3");
-        http = Http.get(toClassic(system));
-    }
+  public Main() {
+    system = ActorSystem.create(Behaviors.empty(), "MqttHttpToS3");
+    http = Http.get(toClassic(system));
+  }
 
-    final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-    final ObjectReader downloadCommandReader = mapper.readerFor(DownloadCommand.class);
+  final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+  final ObjectReader downloadCommandReader = mapper.readerFor(DownloadCommand.class);
 
-    final String mqttBroker = "tcp://localhost:1883";
-    // Remember to set up topic in MQTT server's acl config
-    final String topic = "downloads/trigger";
-    final String s3Bucket = "pekko.connectors.samples";
+  final String mqttBroker = "tcp://localhost:1883";
+  // Remember to set up topic in MQTT server's acl config
+  final String topic = "downloads/trigger";
+  final String s3Bucket = "pekko.connectors.samples";
 
-    private String createS3BucketKey(DownloadCommand info) {
-        return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-            + Uri.create(info.url).getPathString().replace("/", "-");
-    }
+  private String createS3BucketKey(DownloadCommand info) {
+    return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        + Uri.create(info.url).getPathString().replace("/", "-");
+  }
 
-    void run() throws Exception {
-        final MqttConnectionSettings mqttConnectionSettings =
-                MqttConnectionSettings
-                        .create(
-                                mqttBroker,
-                                "upload-control",
-                                new MemoryPersistence()
-                        );
+  void run() throws Exception {
+    final MqttConnectionSettings mqttConnectionSettings =
+        MqttConnectionSettings
+            .create(
+                mqttBroker,
+                "upload-control",
+                new MemoryPersistence()
+            );
 
-        MqttSubscriptions mqttSubscriptions =
-                MqttSubscriptions.create(topic, MqttQoS.atLeastOnce());
+    MqttSubscriptions mqttSubscriptions =
+        MqttSubscriptions.create(topic, MqttQoS.atLeastOnce());
 
-        MqttSource
-                .atMostOnce(mqttConnectionSettings, mqttSubscriptions, 8)
-                .map(m -> m.payload().utf8String())
-                .<DownloadCommand>map(downloadCommandReader::readValue)
-                .mapAsync(4, info -> {
-                            String s3BucketKey = createS3BucketKey(info);
-                            return Source.single(info.url)
-                                    .map(HttpRequest::GET)
-                                    .mapAsync(1, http::singleRequest)
-                                    .flatMapConcat(httpResponse -> httpResponse.entity().getDataBytes())
-                                    .runWith(S3.multipartUpload(s3Bucket, s3BucketKey, ContentTypes.TEXT_HTML_UTF8), system);
-                        }
-                )
-                .runForeach(System.out::println, system)
-                .exceptionally(e -> { e.printStackTrace(); return Done.done(); });
-    }
+    MqttSource
+        .atMostOnce(mqttConnectionSettings, mqttSubscriptions, 8)
+        .map(m -> m.payload().utf8String())
+        .<DownloadCommand>map(downloadCommandReader::readValue)
+        .mapAsync(4, info -> {
+              String s3BucketKey = createS3BucketKey(info);
+              return Source.single(info.url)
+                  .map(HttpRequest::GET)
+                  .mapAsync(1, http::singleRequest)
+                  .flatMapConcat(httpResponse -> httpResponse.entity().getDataBytes())
+                  .runWith(S3.multipartUpload(s3Bucket, s3BucketKey, ContentTypes.TEXT_HTML_UTF8), system);
+            }
+        )
+        .runForeach(System.out::println, system)
+        .exceptionally(e -> {
+          e.printStackTrace();
+          return Done.done();
+        });
+  }
 
 }
