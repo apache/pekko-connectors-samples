f2ac5bc9750ea1ce7608957bfdaac588be65e79b
PJ Fanning <pjfanning@users.noreply.github.com>
2025-01-15 22:10:58 +0100

    upgrade pekko (1.1), sbt, logback, jackson

--- pekko-connectors-sample-jms/project/Dependencies.scala
+++ pekko-connectors-sample-jms/project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Dependencies {
-  val scalaVer = "2.13.14"
+  val scalaVer = "2.13.16"
   // #deps
-  val PekkoVersion = "1.0.3"
+  val PekkoVersion = "1.1.3"
   val PekkoHttpVersion = "1.0.1"
-  val PekkoConnectorsVersion = "1.0.2"
+  val PekkoConnectorsVersion = "1.1.0"
 
   // #deps
 
@@ -20,7 +20,7 @@ object Dependencies {
     "javax.jms" % "jms" % "1.1", // CDDL Version 1.1
     // Logging
     "org.apache.pekko" %% "pekko-slf4j" % PekkoVersion,
-    "ch.qos.logback" % "logback-classic" % "1.2.13",
+    "ch.qos.logback" % "logback-classic" % "1.3.15",
     // #deps
     // http://activemq.apache.org/download.html
     "org.apache.activemq" % "activemq-all" % "5.16.7" exclude("log4j", "log4j") exclude("org.slf4j", "slf4j-log4j12") // ApacheV2
